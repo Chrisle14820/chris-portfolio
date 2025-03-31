9964fe23bae9fd86139acf99bee4fa990b4d422d
Chrisle14820 <chris14820@gmail.com>
2025-03-31 17:37:00 -0500

    feat: add profile pic

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,10 @@ function About() {
     <div className="about-section">
       <div className="profile">
         <div className="profile-pic">
-          <img src="src/assets/profile-pic.jpg" alt="Profile picture" />
+          <img
+            src="https://i.postimg.cc/q7RhChZh/DSC02019.jpg"
+            alt="Profile picture"
+          />
         </div>
         <div className="name">Chris Le</div>
         <div className="job-title">
--- src/components/RowCard/RowCard.jsx
+++ src/components/RowCard/RowCard.jsx
@@ -5,11 +5,6 @@ function RowCard({ children }) {
   return (
     <>
       <div className="row-card-container">
-        <div className="left-container">
-          <a href="aa.com">
-            <img src="src/assets/AALogo.png" alt="company logo" />
-          </a>
-        </div>
         <div className="right-container">{children}</div>
       </div>
     </>
