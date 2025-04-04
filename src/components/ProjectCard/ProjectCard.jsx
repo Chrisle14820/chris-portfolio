982f13c1abb38e6995d7355399ce39513e7abace
Chrisle14820 <chris14820@gmail.com>
2025-04-04 15:56:29 -0500

    fix: clean up styles for responsivness and simplified fetch call

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -6,16 +6,15 @@ import Tag from "../Tag";
 
 function ProjectCard() {
   const [repoData, setRepoData] = useState([]);
-  const [languages, setLanguages] = useState({});
 
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
-        const response = await fetch("https://api.github.com/users/Chrisle14820/repos?sort=updated");
+        const response = await fetch(
+          "https://api.github.com/users/Chrisle14820/repos?sort=updated"
+        );
 
         const data = await response.json();
-
-        console.log(data);
 
         // format data from Repository object
         const formatData = data.map((repo) => ({
@@ -25,75 +24,47 @@ function ProjectCard() {
           desc: repo.description,
           fork_count: repo.forks_count,
           watch_count: repo.watchers_count,
-          lang_url: repo.languages_url,
+          languages: repo.language,
         }));
         setRepoData(formatData); // bind formatted data to repoData useState
       } catch (e) {
         console.log("Error in fetchData", e);
       }
-    }
+    };
     fetchData();
-  },[]);
-
-  const getLanguages = async (repoId, langUrl) => {
-    try {
-      const response = await fetch(langUrl);
-      const data = await response.json();
-  
-      const langText = Object.keys(data);
-
-      // Store the languages for the specific repository
-      setLanguages((prevLanguages) => ({
-        ...prevLanguages,
-        [repoId]: langText, // Use the repo ID as the key
-      }));
-       
-    } catch(e) {
-      console.log("error in getLanguages", e);
-    }
-  }
-
-  const RenderLanguages = ({ repoId }) => {
-    const langText = languages[repoId] || []; // store value of key repoId
-
-    return (
-      <>
-      {langText.map((lang) => (
-        <Tag key={lang} name={lang} />
-      ))}
-      </>
-    )
-  };
+  }, []);
 
   return (
     <>
       {repoData.map((repo) => {
-        if (!languages[repo.id]) {
-          getLanguages(repo.id, repo.lang_url);
-        }
-
         return (
           <div className="project-card-container">
             <div className="project-group">
-            <div className="project-title">
-              <VscRepo />
-              <a href={repo.url}>{repo.name}</a>
+              <div className="project-title">
+                <VscRepo />
+                <a href={repo.url}>{repo.name}</a>
+              </div>
+              <div className="project-body">
+                <p>{repo.desc}</p>
+              </div>
             </div>
-          <div className="project-body">
-            <p>{repo.desc}</p>
+            <div className="project-footer">
+              <div className="project-lang">
+                <Tag name={repo.languages} />
+              </div>
+              <div className="project-info">
+                <span className="fork-count">
+                  <CgGitFork size={16} />
+                  <p>{repo.fork_count}</p>
+                </span>
+                <span className="watch-count">
+                  <CgEye size={16} />
+                  <p>{repo.watch_count}</p>
+                </span>
+              </div>
+            </div>
           </div>
-            </div>
-          <div className="project-footer">
-            <div className="project-lang">
-              <RenderLanguages repoId={repo.id}/>
-            </div>
-            <div className="project-info">
-              <span className="fork-count"><CgGitFork size={16}/><p>{repo.fork_count}</p></span>
-              <span className="watch-count"><CgEye size={16}/><p>{repo.watch_count}</p></span>
-            </div>
-          </div>
-        </div>
-        )
+        );
       })}
     </>
   );
